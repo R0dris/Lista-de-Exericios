c655b6c5010edb429741526e887865024ac6cd60
João Vitor <joaovitorrods089@gmail.com>
2024-05-03 14:47:15 -0300

    feat: Exercício 3 finalizado

---
+++ ex3.js
@@ -0,0 +1,16 @@
+/*Faça um programa que receba um número do usuário e informe se este número é par ou
+ímpar.
+*/
+
+let numero = parseInt (prompt ("Escolha um número"))
+
+if (numero%2 == 0)
+{
+    console.log ("Número par inserido")
+}
+else 
+{
+    console.log ("Número impar inserido")
+}
+
+
